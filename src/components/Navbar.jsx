1b7900ecdb9dfe03ba35a49e30d203f2f050df1d
egamovj <egamovj90@gmail.com>
2024-02-28 22:18:34 +0500

    navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,12 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to={"/"}>Cryptoverse</Link>
         </Typography.Title>
-        <Button className="menu-control-container"></Button>
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link></Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
