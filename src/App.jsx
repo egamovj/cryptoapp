ea25573931502e0110474899720566e41aa80df0
egamovj <egamovj90@gmail.com>
2024-02-28 22:11:35 +0500

    navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+
+        <div className="main"></div>
+
+        <div className="footer"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
